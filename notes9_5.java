b308cc86b834d20404ed959f5fe8757059b64966
Rayncao <112970360+Rayncao@users.noreply.github.com>
2024-09-05 14:04:43 -0700

    uploading notes

---
+++ notes9_5.java
@@ -0,0 +1,15 @@
+class notes9_5{
+	public static void main(String[] args) {
+		//Data types
+		//defined an integer variable with a value of 4
+		int x = 4;
+		//redefine the variable. set it equal to 6
+		x = 6;
+
+		//cast different types onto existing data
+
+		float y = (float)x;
+
+		System.out.println(y);
+	}
+}
