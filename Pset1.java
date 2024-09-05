fcc7a92243c24de03b6c20b39bd3f8219941a85f
Rayncao <112970360+Rayncao@users.noreply.github.com>
2024-09-05 14:09:36 -0700

    uploading notes

---
+++ Pset1.java
@@ -0,0 +1,8 @@
+class Pset1 {
+	public static void main(String[] args) {
+		System.out.print("Ryan Cao is a student in IB CS");
+		System.out.print("Q1: Python is a interpreter which translates everything and executes at the same time while Java is a compiler which translates everything first, and then you can execute it");
+		System.out.print("Q2: An assembly language is used to directly communicate with a computer's hardware, high-level languages are computer languages comprehensible to human needing translation into low level languages in order for the computer to understand");
+		System.out.print("Compliers are used to translate high-level languages written by human into coputer-comprehensible machine codes");
+	}
+}
